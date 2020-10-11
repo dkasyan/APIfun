a8389be412b7f88b7bcc9cd6ceb9e459485033e9
Damian Kasyan <damian.kasyan@hotmail.com>
2020-10-11 19:34:21 +0200

    Problem

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 import requests
 import csv
+import json
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-print(data)
+data = response.json()[0]
+data_dict = data.keys()
+print(type(data))
+print(data_dict)
+#print(data_dict['rates'])
+#print(data.dumps("\"table\currency")
+#print(data.keys())
 
-with open('curency.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar=';', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['currency', 'code', 'bid', 'ask'])
+#print(data["currency"])
+
+
+
+#data = {"data": {
+#  "id": 1,
+#  "name": "Something",
+#  "colors": ["red", "blue"]
+#  }
+#}
+
+#print(type(data))
+# <class 'dict'>
+
+
+
+
+### To do zapisu
+#with open('curency.csv', 'w', newline='') as csvfile:
+#    spamwriter = csv.writer(csvfile, delimiter=' ',
+#                            quotechar=';', quoting=csv.QUOTE_MINIMAL)
+#    spamwriter.writerow(['currency', 'code', 'bid', 'ask'])
