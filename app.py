2a93dc3bab0162f13e7a4100675e24bbdb443a90
Damian Kasyan <damian.kasyan@hotmail.com>
2020-10-11 18:51:12 +0200

    curency

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import requests
+import csv
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+print(data)
+
+with open('curency.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar=';', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['currency', 'code', 'bid', 'ask'])
