e41a0085d42b8021afcc143b05692223f7f05f8f
Damian Kasyan <dkasyan@novomatic-tech.com>
2020-10-13 10:24:49 +0200

    To fix

--- app.py
+++ app.py
@@ -22,37 +22,26 @@ def outputing_data(currency, code, bid, ask):
 
 
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()[0]
-new_data = data['rates']
-print(type(new_data))
-#print(new_data)
+#response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+#data = response.json()[0]
+#new_data = data['rates']
+##print(type(new_data))
+##print(new_data)
 
-data_structure()
-for i in new_data:
-    outputing_data(i["currency"], i["code"], i["bid"], i["ask"])
+#data_structure()
+#for i in new_data:
+#    outputing_data(i["currency"], i["code"], i["bid"], i["ask"])
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def message():
-    if request.method == "GET":
-        data = request.form
-        currency = data.get('currency')
-        author = data.get("author")
-        print(data)
-        print(currency)
-        print(author)
+    if request.method == "POST":
+        print("dupa")
+        datas = request.form
+        currency = datas.get('currency')
+        author = datas.get("author")
+        print(datas)
 
-   # print(request.form)
     return render_template("cc_form.html")
-   # return redirect('/cc_form.html')
-
-
-
-### To do zapisu
-#with open('curency.csv', 'w', newline='') as csvfile:
-#    spamwriter = csv.writer(csvfile, delimiter=' ',
-#                            quotechar=';', quoting=csv.QUOTE_MINIMAL)
-#    spamwriter.writerow(['currency', 'code', 'bid', 'ask'])
-
+     
